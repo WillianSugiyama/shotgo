6b0f4864a8538b327491c7d19c563dc9b110ab5b
Willian Sugiyama <williansugiyama@gmail.com>
2026-04-06 14:59:28 -0300

    fix: retain NSStatusItem and use SF Symbol for tray icon
    NSStatusItem was being garbage collected. Now retained by delegate.
    Uses camera.viewfinder SF Symbol as template icon (falls back to "SG").
    Delegate owns the status item lifecycle for proper cleanup.

--- internal/app/tray_darwin.m
+++ internal/app/tray_darwin.m
@@ -3,49 +3,71 @@
 #include "_cgo_export.h"
 
 @interface SGTrayDelegate : NSObject
+- (void)menuAction:(NSMenuItem *)sender;
+- (void)setupTray;
 @end
 
-@implementation SGTrayDelegate
+@implementation SGTrayDelegate {
+    NSStatusItem *_item;
+}
+
 - (void)menuAction:(NSMenuItem *)sender {
     goTrayCallback((int)sender.tag);
 }
+
+- (void)setupTray {
+    _item = [[[NSStatusBar systemStatusBar]
+        statusItemWithLength:NSSquareStatusItemLength] retain];
+
+    NSImage *img = [NSImage imageWithSystemSymbolName:@"camera.viewfinder"
+        accessibilityDescription:@"ShotGo"];
+    if (img) {
+        [img setTemplate:YES];
+        _item.button.image = img;
+    } else {
+        _item.button.title = @"SG";
+    }
+
+    NSMenu *m = [[NSMenu alloc] init];
+    [self addTo:m title:@"Capture Fullscreen" tag:1 key:@""];
+    [self addTo:m title:@"Capture Region" tag:2 key:@""];
+    [m addItem:[NSMenuItem separatorItem]];
+    [self addTo:m title:@"Record Screen" tag:3 key:@""];
+    [m addItem:[NSMenuItem separatorItem]];
+    [self addTo:m title:@"Settings" tag:4 key:@","];
+    [self addTo:m title:@"Quit ShotGo" tag:5 key:@"q"];
+    _item.menu = m;
+    fprintf(stderr, "[shotgo] tray: status item visible\n");
+}
+
+- (void)addTo:(NSMenu *)m title:(NSString *)t tag:(int)tag key:(NSString *)k {
+    NSMenuItem *i = [[NSMenuItem alloc] initWithTitle:t
+        action:@selector(menuAction:) keyEquivalent:k];
+    i.tag = tag;
+    i.target = self;
+    [m addItem:i];
+}
+
+- (void)removeTray {
+    if (_item) {
+        [[NSStatusBar systemStatusBar] removeStatusItem:_item];
+        [_item release];
+        _item = nil;
+    }
+}
 @end
 
-static NSStatusItem *_sgItem = nil;
 static SGTrayDelegate *_sgDel = nil;
 
 void SGSetupTray(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
         _sgDel = [[SGTrayDelegate alloc] init];
-        _sgItem = [[NSStatusBar systemStatusBar]
-            statusItemWithLength:NSVariableStatusItemLength];
-        _sgItem.button.title = @"SG";
-        NSMenu *m = [[NSMenu alloc] init];
-
-        void (^add)(NSString *, int, NSString *) =
-            ^(NSString *t, int tag, NSString *ke) {
-            NSMenuItem *i = [[NSMenuItem alloc] initWithTitle:t
-                action:@selector(menuAction:) keyEquivalent:ke];
-            i.tag = tag;
-            i.target = _sgDel;
-            [m addItem:i];
-        };
-
-        add(@"Capture Fullscreen", 1, @"");
-        add(@"Capture Region", 2, @"");
-        [m addItem:[NSMenuItem separatorItem]];
-        add(@"Record Screen", 3, @"");
-        [m addItem:[NSMenuItem separatorItem]];
-        add(@"Settings", 4, @",");
-        add(@"Quit ShotGo", 5, @"q");
-        _sgItem.menu = m;
-        fprintf(stderr, "[shotgo] tray icon created\n");
+        [_sgDel setupTray];
     });
 }
 
 void SGRemoveTray(void) {
-    if (_sgItem) {
-        [[NSStatusBar systemStatusBar] removeStatusItem:_sgItem];
-        _sgItem = nil;
+    if (_sgDel) {
+        [_sgDel removeTray];
     }
 }
